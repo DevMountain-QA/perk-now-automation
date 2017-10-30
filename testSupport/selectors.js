f5ae99d03d95655ea38c5fbb5a0c6b78624aab38
Tyler Dallinga <tyler.dallinga@gmail.com>
2017-10-30 10:58:10 -0600

    tylers selectors

--- testSupport/selectors.js
+++ testSupport/selectors.js
@@ -9,7 +9,10 @@ module.exports = {
             },
             featuredDeal: ''
         },
-        deals: {
+        vendors: {
+            vendorListItem: 'div[class=vendors-list]'
+
+
 
         },
         dealRedemption: {
@@ -23,6 +26,41 @@ module.exports = {
             forgotPasswordLink: ''
         },
         accountCreation: {
+            haveWorkEmail: {
+                yesButton:'button[ng-click="goToSignupEmail()"',
+                noCodebutton:'button[ng-click="goToSignupCode()"',
+                privacyPolicy:'a[class=legalLinks]',
+                termAndConditions:'a[ui-sref=tos]'                
+            },
+            yesEmailScreen: {
+                workEmailField:'input[type=email]'
+            },
+            noCodeScreen: {
+                codeBox1:'input[ng-model="user.company_code1"]',
+                codeBox2:'input[ng-model="user.company_code2"]',
+                codeBox3:'input[ng-model="user.company_code3"]',
+                codeBox4:'input[ng-model="user.company_code4"]',
+                personalEmail:'input[type=email]'
+            },
+            insertInfoScreen:{
+                firstName:'input[placeholder=First Name]',
+                lastName:'input[placeholder=Last Name]',
+                password:'input[placeholder=Password]',
+                showPassword:'input[type=checkbox]'
+            },
+            zipCode:{
+                enterZip:'input[type=number]',
+                getPerksButton:'button[id=account-create]'
+            },
+            progressBar: {
+                backButtonHome:'div[class=backArrow button icon-left ion-chevron-left button-clear]',
+                backbutton:'div[id=backArrowLeft]',
+                forwardButton:'div[id=backArrowRight]',
+                firstProgressButton:'a[ui-sref="signupLanding"]',
+                //secondProgressButton:'', //not enough info to pull selecor
+                thirdProgressButton:'a[ui-sref="signupInfo"]',
+                fourthProgressButton:'a[ui-sref="signupZip"]'
+            }
 
         },
         menuBar: {
@@ -50,6 +88,12 @@ module.exports = {
 
         },
         transactionScreen: {
+            transactionTag:'td',
+            transactionText1:'6',
+            transactionText2:'15',
+            transactionModifyScreen:{
+                //there is a bug present, not allwoing you to view the individual transaction.
+            }
 
         }
     }
