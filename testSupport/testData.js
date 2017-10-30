29a8e76fb6bf9387573dd08816bac86e2aa9a5bf
AJ Larson <aj.larson@devmounta.in>
2017-10-30 15:57:25 -0600

    fixed formatting

--- testSupport/testData.js
+++ testSupport/testData.js
@@ -1,5 +1,5 @@
 module.exports = {
-    signIn : { 
+    signIn: {
         username: 'admin@perknow.com',
         password: 'whatever',
 
@@ -11,8 +11,8 @@ module.exports = {
         password2: 'test1pass',
 
         badUsername: '',
-        badPassword: '' 
-                
+        badPassword: ''
+
     },
     accountCreation: {
         goodData: {
@@ -55,35 +55,35 @@ module.exports = {
 
 
     },
-    deals : {
-        category : {
-            fun : {
-                deal1 : {
-                    vendor:'Fun 1',
-                    title : 'Gameboy' ,
-                    description : 'Free Gameboy',
-                    location : '123 NJHUYTG St KLN, UT 84043',
-                    hours:'Weekday hours: 9-5 Saturday hours: 9-5 Sunday hours: Closed'
+    deals: {
+        category: {
+            fun: {
+                deal1: {
+                    vendor: 'Fun 1',
+                    title: 'Gameboy',
+                    description: 'Free Gameboy',
+                    location: '123 NJHUYTG St KLN, UT 84043',
+                    hours: 'Weekday hours: 9-5 Saturday hours: 9-5 Sunday hours: Closed'
                 },
-                deal2:{
-                    vendor:'Fun 3',
-                    title : '15 50% off' ,
-                    description : '15 Come in today for 50% off Fun 3',
-                    location : '123 NJHUYTG St KLN, UT 84043',
-                    hours:'Weekday hours: 9-5 Saturday hours: 9-5 Sunday hours: Closed' 
+                deal2: {
+                    vendor: 'Fun 3',
+                    title: '15 50% off',
+                    description: '15 Come in today for 50% off Fun 3',
+                    location: '123 NJHUYTG St KLN, UT 84043',
+                    hours: 'Weekday hours: 9-5 Saturday hours: 9-5 Sunday hours: Closed'
                 }
             },
-            food : {
-                food1 : {
-                    title : '3 20% off',
-                    vendor : 'Food 1',
-                    deal : '3 20% off',
-                    description : '3 Come in today for 20% off Food 1',
-                    location : '123 NJHUYTG St KLN, UT 84043', 
-                    hours : 'Weekday hours: 9-5, Saturday hours: 9-5, Sunday hours: Closed',
-                Concierge4 : {
-                    title : 'free food', 
-                }
+            food: {
+                food1: {
+                    title: '3 20% off',
+                    vendor: 'Food 1',
+                    deal: '3 20% off',
+                    description: '3 Come in today for 20% off Food 1',
+                    location: '123 NJHUYTG St KLN, UT 84043',
+                    hours: 'Weekday hours: 9-5, Saturday hours: 9-5, Sunday hours: Closed',
+                    Concierge4: {
+                        title: 'free food',
+                    }
 
                 }
 
