738740b84a6cb56cde2d49062c7a0c07e3a012b0
AJ Larson <aj.larson@devmounta.in>
2017-10-30 15:58:34 -0600

    fixed object

--- testSupport/testData.js
+++ testSupport/testData.js
@@ -81,13 +81,14 @@ module.exports = {
                     description: '3 Come in today for 20% off Food 1',
                     location: '123 NJHUYTG St KLN, UT 84043',
                     hours: 'Weekday hours: 9-5, Saturday hours: 9-5, Sunday hours: Closed',
-                    Concierge4: {
-                        title: 'free food',
-                    }
-
+                },
+                Concierge4: {
+                    title: 'free food',
                 }
 
             }
+
         }
     }
+}
 }
