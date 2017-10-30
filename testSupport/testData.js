109cb88a036280bb4f63352d7f4bc513b8a65cdf
Orton <zackorton@rock.local>
2017-10-30 15:41:57 -0600

    data rocks

--- testSupport/testData.js
+++ testSupport/testData.js
@@ -15,6 +15,18 @@ module.exports = {
                 }
             },
             food : {
+                food1 : {
+                    title : '3 20% off',
+                    vendor : 'Food 1',
+                    deal : '3 20% off',
+                    description : '3 Come in today for 20% off Food 1',
+                    location : '123 NJHUYTG St KLN, UT 84043', 
+                    hours : 'Weekday hours: 9-5, Saturday hours: 9-5, Sunday hours: Closed',
+                Concierge4 : {
+                    title : 'free food', 
+                }
+
+                }
 
             }
         }
