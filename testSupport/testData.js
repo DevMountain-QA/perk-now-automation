88723d991a7a74d699b3b7c1c0f931c9ef1651f4
AJ Larson <aj.larson@devmounta.in>
2017-10-30 15:11:38 -0600

    test data setup

--- testSupport/testData.js
+++ testSupport/testData.js
@@ -1,3 +1,22 @@
 module.exports = {
-    
+    signIn : {
+
+    },
+    accountCreation : {
+
+    },
+    deals : {
+        category : {
+            fun : {
+                deal1 : {
+                    title : '' ,
+                    description : '',
+                    email : ''
+                }
+            },
+            food : {
+
+            }
+        }
+    }
 }
