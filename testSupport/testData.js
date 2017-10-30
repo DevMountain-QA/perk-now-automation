f4ed5e9254317bdb85c111c565659f8e216e81c0
Teri Larson <terijolarson2016@gmail.com>
2017-10-30 15:40:55 -0600

    updated data

--- testSupport/testData.js
+++ testSupport/testData.js
@@ -1,20 +1,58 @@
 module.exports = {
-    signIn : {
+    signIn: {
 
     },
-    accountCreation : {
+    accountCreation: {
+        goodData: {
+            account1: {
+                haveWorkEmail: 'Yes',
+                workEmail: 'tjbob@devmountain.com',
+                firstname: 'TJ',
+                lastname: 'Bob',
+                password: 'whatever',
+                zipCode: '48568'
+            },
+            account2: {
+                haveWorkEmail: 'No',
+                workCode: 'code',
+                firstname: 'Josephine',
+                lastname: 'Loveless',
+                password: 'whatever',
+                zipCode: '84111'
+            }
+        },
+        badData: {
+            account1B: {
+                haveWorkEmail: 'Yes',
+                workEmail: 'tjbob@juno.com',
+                firstname: 'TJ',
+                lastname: 'Bob',
+                password: 'test1Fail',
+                zipCode: '84111'
+            },
+            account2B: {
+                haveWorkEmail: 'No',
+                workCode: '4312',
+                firstname: 'tj',
+                lastname: 'bobby',
+                password: 'test2Fail',
+                zipCode: '84100'
+
+            },
+        }
+
 
     },
-    deals : {
-        category : {
-            fun : {
-                deal1 : {
-                    title : '' ,
-                    description : '',
-                    email : ''
+    deals: {
+        category: {
+            fun: {
+                deal1: {
+                    title: '',
+                    description: '',
+                    email: ''
                 }
             },
-            food : {
+            food: {
 
             }
         }
