24a70fbaa566bfbbb0abec4ea1e9e5428d11a114
AJ Larson <aj.larson@devmounta.in>
2017-10-31 10:02:10 -0600

    updated comment on command

--- customCommands/checkAttributeByText.js
+++ customCommands/checkAttributeByText.js
@@ -1,6 +1,6 @@
 // Gets back an array of elements containing DOM 'selector', then gets the text of each. Runs through a loop to check
-// 'text' parameter against each element's text. If the element's text matches the 'text' parameter, we click that
-// element and break the loop.
+// 'text' parameter against each element's text. If the element's text matches the 'text' parameter, we check attribute
+// of the element and break the loop.
 exports.command = function (selector, text, attr, value) {
     this.elements('css selector', selector, function (links) {
       let success = false;
